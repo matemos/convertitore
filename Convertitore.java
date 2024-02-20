14942ab61ce1bb57665adde23dc13a629bfc33d2
Marco Cerbella <Marco Cerbella>
2024-02-20 11:04:02 +0100

    primo commit

---
+++ Convertitore.java
@@ -0,0 +1,17 @@
+package it.edu.iisgubbio.convertitore;
+
+import javafx.application.Application;
+import 
+
+public class Convertitore extends Application {
+
+	public void start(Stage stage) {
+		
+	}
+	
+	public static void main(String[] args) {
+		launch(args);
+
+	}
+
+}
